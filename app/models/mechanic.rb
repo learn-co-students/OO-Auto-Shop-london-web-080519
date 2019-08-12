0f8a4b6d7092112fbfa93f35359cbb178c414d45
github username <github email address>
2019-08-12 17:22:02 +0100

    joaquin completed OO Relationship Practice: Auto Shop

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,28 @@
 class Mechanic
 
+  @@all = []
+
   attr_reader :name, :specialty
+  
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def self.all
+    @@all
+  end
 
+  def cars
+    Car.all.select{|car| car.mechanic == self}
+  end
 
+  def car_owners
+    cars.map{|car| car.owner}
+  end
+
+  def car_owner_names
+    car_owners.map{|car_owner| car_owner.name}
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,20 @@ def reload
   load 'config/environment.rb'
 end
 
+
+car_owner1 = CarOwner.new("jeff")
+car_owner2 = CarOwner.new("john")
+
+mechanic1 = Mechanic.new("giusepe", "antique")
+mechanic2 = Mechanic.new("manu", "exotic")
+mechanic3 = Mechanic.new("joe", "clunker")
+mechanic4 = Mechanic.new("jack", "clunker")
+
+car1 = Car.new("ferrari", "enzo", "antique", car_owner1, mechanic1)
+car2 = Car.new("pontiac", "gto", "exotic", car_owner1, mechanic2)
+car3 = Car.new("vw", "golf", "clunker", car_owner2, mechanic3)
+car4 = Car.new("porsche", "carrera", "exotic", car_owner2, mechanic2)
+
+
+
 binding.pry
