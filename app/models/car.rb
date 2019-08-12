0617e25583495fdc5439153444d2fd9724fdc755
github username <aude53@gmail.com>
2019-08-12 17:49:30 +0100

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,31 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :owner, :mechanic, :classification
 
-  def initialize(make, model, classification)
+  @@all = []
+
+  def initialize(make, model, classification, owner, mechanic)
     @make = make
     @model = model
+    @owner = owner
+    @mechanic = mechanic
     @classification = classification
+    @@all << self
+  end
+
+  # Get a list of all cars
+  def self.all
+    @@all
+  end
+
+  # Get a list of all car classifications
+  def self.classifications
+    self.all.map{|car| car.classification}.uniq
+  end
+
+  # Get a list of mechanics that have an expertise that matches the car classification
+  def matching_expertise
+    Mechanic.all.select{|mechanic| mechanic.specialty == self.classification}
   end
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,10 +2,32 @@ class Mechanic
 
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  @@all = []
 
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
+
+  # Get a list of all mechanics
+  def self.all
+    @@all
+  end
+
+  # Get a list of all cars that a mechanic services
+  def cars
+    Car.all.select{|car| car.mechanic == self}
+  end
+
+  # Get a list of all the car owners that go to a specific mechanic
+  def owners
+    cars.map{|car| car.owner}.uniq
+  end
+
+  # Get a list of the names of all car owners who go to a specific mechanic
+  def owner_names
+    owners.map{|owner| owner.name}
+  end
 
 end
