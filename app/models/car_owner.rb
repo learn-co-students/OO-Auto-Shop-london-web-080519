1904602cd5fe37b7414e91d62b61ba060c38e480
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-08-13 16:46:15 +0100

    CarOwner methods

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -12,5 +12,21 @@ class CarOwner
     @@all
   end
 
+  def get_my_cars
+    Car.all.select{|car| car.owner == self}
+  end
+
+  def show_my_cars
+    get_my_cars.map{|car| "#{car.make} - #{car.model}, #{car.classification}."}
+  end
+
+  def my_mechanics
+    get_my_cars.map{|car| "#{car.mechanic.name}, for the #{car.make} - #{car.model}." }
+  end
+
+  def self.average_cars
+    allcars = self.all.map {|owner| owner.get_my_cars.count }
+    allcars.reduce(:+) / allcars.count.to_f
+  end
 
 end
