4f60298189795f43118ee2ac2d64318dec3997fe
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-08-13 17:02:38 +0100

    Finished Mechanic methods

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -13,6 +13,20 @@ class Mechanic
     @@all
   end
 
-  
+  def get_my_cars
+    Car.all.select{|car| car.mechanic == self}
+  end
+
+  def show_my_cars
+    get_my_cars.map{|car| "#{car.make} - #{car.model}. "}
+  end
+
+  def car_owners
+    get_my_cars.map{|car| car.owner }
+  end
+
+  def car_owners_names
+    car_owners.map{|owner| owner.name}
+  end
 
 end
